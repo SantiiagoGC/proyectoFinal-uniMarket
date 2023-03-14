ac0a877c8321c7a40128d040339d7284b07855ee
nodiersolano <solanonodier@gmail.com>
2023-03-13 23:10:25 -0500

    Empezando las relaciones por Nodier

--- persistencia/src/main/java/co/edu/uniquindio/uniMarket/entidades/Comentario.java
+++ persistencia/src/main/java/co/edu/uniquindio/uniMarket/entidades/Comentario.java
@@ -1,9 +1,31 @@
 package co.edu.uniquindio.uniMarket.entidades;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Comentario {
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Comentario  implements Serializable {
+    @ManyToOne
+    @JoinColumn(nullable = false)
     private Usuario usuario;
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Producto producto;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private String id;
+    @Column(nullable = false)
     private LocalDate fecha;
+
 }
